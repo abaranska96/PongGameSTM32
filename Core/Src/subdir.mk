9da7b01a29b4e46c7a5325efd0944077e2a68355
abaranska96 <42865247+abaranska96@users.noreply.github.com>
2023-10-26 11:08:57 +0200

    from local to remote

---
+++ Core/Src/subdir.mk
@@ -0,0 +1,117 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/adc.c \
+../Core/Src/crc.c \
+../Core/Src/dma.c \
+../Core/Src/dma2d.c \
+../Core/Src/fmc.c \
+../Core/Src/fonts.c \
+../Core/Src/freertos.c \
+../Core/Src/gpio.c \
+../Core/Src/i2c.c \
+../Core/Src/ltdc.c \
+../Core/Src/spi.c \
+../Core/Src/stm32f4xx_hal_msp.c \
+../Core/Src/stm32f4xx_hal_timebase_tim.c \
+../Core/Src/stm32f4xx_it.c \
+../Core/Src/syscalls.c \
+../Core/Src/sysmem.c \
+../Core/Src/system_stm32f4xx.c \
+../Core/Src/tim.c \
+../Core/Src/usart.c 
+
+CPP_SRCS += \
+../Core/Src/digital_input.cpp \
+../Core/Src/digital_output.cpp \
+../Core/Src/display.cpp \
+../Core/Src/dwt_stm32_delay.cpp \
+../Core/Src/ili9341.cpp \
+../Core/Src/ili9341_touch.cpp \
+../Core/Src/joystick.cpp \
+../Core/Src/keyboard.cpp \
+../Core/Src/main.cpp \
+../Core/Src/pong.cpp 
+
+C_DEPS += \
+./Core/Src/adc.d \
+./Core/Src/crc.d \
+./Core/Src/dma.d \
+./Core/Src/dma2d.d \
+./Core/Src/fmc.d \
+./Core/Src/fonts.d \
+./Core/Src/freertos.d \
+./Core/Src/gpio.d \
+./Core/Src/i2c.d \
+./Core/Src/ltdc.d \
+./Core/Src/spi.d \
+./Core/Src/stm32f4xx_hal_msp.d \
+./Core/Src/stm32f4xx_hal_timebase_tim.d \
+./Core/Src/stm32f4xx_it.d \
+./Core/Src/syscalls.d \
+./Core/Src/sysmem.d \
+./Core/Src/system_stm32f4xx.d \
+./Core/Src/tim.d \
+./Core/Src/usart.d 
+
+OBJS += \
+./Core/Src/adc.o \
+./Core/Src/crc.o \
+./Core/Src/digital_input.o \
+./Core/Src/digital_output.o \
+./Core/Src/display.o \
+./Core/Src/dma.o \
+./Core/Src/dma2d.o \
+./Core/Src/dwt_stm32_delay.o \
+./Core/Src/fmc.o \
+./Core/Src/fonts.o \
+./Core/Src/freertos.o \
+./Core/Src/gpio.o \
+./Core/Src/i2c.o \
+./Core/Src/ili9341.o \
+./Core/Src/ili9341_touch.o \
+./Core/Src/joystick.o \
+./Core/Src/keyboard.o \
+./Core/Src/ltdc.o \
+./Core/Src/main.o \
+./Core/Src/pong.o \
+./Core/Src/spi.o \
+./Core/Src/stm32f4xx_hal_msp.o \
+./Core/Src/stm32f4xx_hal_timebase_tim.o \
+./Core/Src/stm32f4xx_it.o \
+./Core/Src/syscalls.o \
+./Core/Src/sysmem.o \
+./Core/Src/system_stm32f4xx.o \
+./Core/Src/tim.o \
+./Core/Src/usart.o 
+
+CPP_DEPS += \
+./Core/Src/digital_input.d \
+./Core/Src/digital_output.d \
+./Core/Src/display.d \
+./Core/Src/dwt_stm32_delay.d \
+./Core/Src/ili9341.d \
+./Core/Src/ili9341_touch.d \
+./Core/Src/joystick.d \
+./Core/Src/keyboard.d \
+./Core/Src/main.d \
+./Core/Src/pong.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F429xx -c -I../Core/Inc -I../USB_HOST/App -I../USB_HOST/Target -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I../Middlewares/ST/STM32_USB_Host_Library/Core/Inc -I../Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Core/Src/%.o: ../Core/Src/%.cpp Core/Src/subdir.mk
+	arm-none-eabi-g++ "$<" -mcpu=cortex-m4 -std=gnu++14 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F429xx -c -I../Core/Inc -I../USB_HOST/App -I../USB_HOST/Target -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I../Middlewares/ST/STM32_USB_Host_Library/Core/Inc -I../Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -O0 -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti -fno-use-cxa-atexit -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core-2f-Src
+
+clean-Core-2f-Src:
+	-$(RM) ./Core/Src/adc.d ./Core/Src/adc.o ./Core/Src/crc.d ./Core/Src/crc.o ./Core/Src/digital_input.d ./Core/Src/digital_input.o ./Core/Src/digital_output.d ./Core/Src/digital_output.o ./Core/Src/display.d ./Core/Src/display.o ./Core/Src/dma.d ./Core/Src/dma.o ./Core/Src/dma2d.d ./Core/Src/dma2d.o ./Core/Src/dwt_stm32_delay.d ./Core/Src/dwt_stm32_delay.o ./Core/Src/fmc.d ./Core/Src/fmc.o ./Core/Src/fonts.d ./Core/Src/fonts.o ./Core/Src/freertos.d ./Core/Src/freertos.o ./Core/Src/gpio.d ./Core/Src/gpio.o ./Core/Src/i2c.d ./Core/Src/i2c.o ./Core/Src/ili9341.d ./Core/Src/ili9341.o ./Core/Src/ili9341_touch.d ./Core/Src/ili9341_touch.o ./Core/Src/joystick.d ./Core/Src/joystick.o ./Core/Src/keyboard.d ./Core/Src/keyboard.o ./Core/Src/ltdc.d ./Core/Src/ltdc.o ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/pong.d ./Core/Src/pong.o ./Core/Src/spi.d ./Core/Src/spi.o ./Core/Src/stm32f4xx_hal_msp.d ./Core/Src/stm32f4xx_hal_msp.o ./Core/Src/stm32f4xx_hal_timebase_tim.d ./Core/Src/stm32f4xx_hal_timebase_tim.o ./Core/Src/stm32f4xx_it.d ./Core/Src/stm32f4xx_it.o ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/system_stm32f4xx.d ./Core/Src/system_stm32f4xx.o ./Core/Src/tim.d ./Core/Src/tim.o ./Core/Src/usart.d ./Core/Src/usart.o
+
+.PHONY: clean-Core-2f-Src
+
